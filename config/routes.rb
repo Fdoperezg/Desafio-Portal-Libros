b9f7295a25e22a572ae14b3ef6b43349e51722c4
Fernando <fp.naldo@gmail.com>
2021-05-05 23:03:29 -0400

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
+  root 'books#index'
+  resources :libros, only: :index do
+    resources :orders, only: :create
+  end
+  resources :orders, only: :index
+  resources :libros
+  delete 'order/:id', to: 'orders#destroy', as: 'destroy_order'
+  delete 'orders/clean', to: 'orders#clean', as: 'clean_orders'
+
+  resources :billings, only: [] do
+    collection do
+      get 'pre-pay'
+      get 'execute'
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
